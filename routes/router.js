5edfd1d2ef476638f256526c1c04b3e01e0ba394
Manikanta662001 <gundlurimanikanta142@gmail.com>
2024-02-27 11:31:49 +0530

    creating of jwt token,login api is created

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const usermodel = require("../models/userSchema");
 const { STATUS_TYPES } = require("../utils/Constants");
@@ -19,13 +20,36 @@ router.post("/register", async (req, res) => {
       incomingdt["password"] = await bcrypt.hash(password, 10);
       incomingdt["cpassword"] = await bcrypt.hash(cpassword, 10);
       const dbres = await usermodel.create(incomingdt);
-      res.status(STATUS_TYPES.CREATED).send(dbres);
+      res
+        .status(STATUS_TYPES.CREATED)
+        .send({ message: "User Registered Successfully" });
     }
   } catch (error) {
     console.log(error);
     return res
       .status(STATUS_TYPES.DUPLICATE_KEY)
       .send({ error: error.message });
+  }
+});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user_or_not = await usermodel.findOne({ email });
+  if (user_or_not) {
+    const passwordmatch = await bcrypt.compare(password, user_or_not.password);
+    if (passwordmatch) {
+  let token = jwt.sign({_id:user_or_not._id},process.env.JWT_SECRET)
+      return res
+        .status(STATUS_TYPES.CREATED)
+        .json({ message: "Login Successfull" ,token});
+    } else {
+      return res
+        .status(STATUS_TYPES.FORBIDDEN)
+        .json({ error: "Password is not matching" });
+    }
+  } else {
+    return res
+      .status(STATUS_TYPES.FORBIDDEN)
+      .json({ error: "Email Not Found" });
   }
 });
 
--- utils/Constants.js
+++ utils/Constants.js
@@ -1,6 +1,7 @@
 const STATUS_TYPES = {
     OK : 200,
     CREATED:201,
-    DUPLICATE_KEY:300,
+    DUPLICATE_KEY:605,
+    FORBIDDEN:403,
 }
 module.exports = {STATUS_TYPES}
